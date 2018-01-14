53b891fe42132b249c740111f9c4ca737bfdd879
Рома <roma219@yandex.ru>
2018-01-14 13:40:16 +0300

    in-component getter naming

--- client/views/User/index.vue
+++ client/views/User/index.vue
@@ -2,13 +2,13 @@
   <div class="page">
     <router-link to="/">back</router-link>
     <div v-if="this.$store.state.user.account">
-       <h4>{{getUserName}} [{{getAccountObject.id}}]</h4>
+       <h4>{{ userName }} [{{ accountObject.id }}]</h4>
        
        <div v-if="this.$store.state.user.balances">
-          <ul v-for="balance in getBalances">
+          <ul v-for="balance in balances">
             <li v-if="balance.balance">
-              {{getAssetFieldById('symbol',balance.asset_type)}}
-              {{drawRealBalance(balance.balance,getAssetFieldById('precision',balance.asset_type))}}
+              {{ getAssetFieldById('symbol',balance.asset_type) }}
+              {{ drawRealBalance(balance.balance,getAssetFieldById('precision',balance.asset_type)) }}
             </li>
           </ul>
        </div>
@@ -23,13 +23,13 @@ export default {
     this.$store.dispatch('fetchUser',this.$route.params.nickname);
   },
   computed: {
-    ...mapGetters([
-      'getUserName',
-      'getAccountObject',
-      'getBalances',
-      'getAssetById',
-      'getAssetFieldById'
-    ])
+    ...mapGetters({
+      userName: 'getUserName',
+      accountObject: 'getAccountObject',
+      balances: 'getBalances',
+      getAssetById: 'getAssetById',
+      getAssetFieldById: 'getAssetFieldById'
+    })
   },
   methods: {
     drawRealBalance(amount,preceision){
