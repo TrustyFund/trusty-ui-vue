70866a6cd124b9380b24570e9d6af59b1c142529
Рома <roma219@yandex.ru>
2018-01-26 10:50:32 +0300

    submodule cleanup

--- client/store/vuex-bitshares/src/getters/assets.js
+++
@@ -1,7 +0,0 @@
-export function getAssetById({ assets }) {
-  return (id) => ((assets && assets[id]) ? assets[id] : false);
-}
-
-export function getAssetFieldById({ assets }) {
-  return (field, id) => ((assets && assets[id]) ? assets[id][field] : false);
-}
--- client/store/vuex-bitshares/src/index.js
+++
@@ -1,9 +0,0 @@
-import apis from './modules/apis';
-import user from './modules/user';
-import assets from './modules/assets';
-
-export default function install(store) {
-  store.registerModule('apis', apis);
-  store.registerModule('user', user);
-  store.registerModule('assets', assets);
-}
