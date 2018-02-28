924816c8da9a10e2166c855caf6d9edcae484fae
Anton <anlopan@gmail.com>
2018-02-28 10:53:46 +0300

    Rename fields, add paddings

--- client/components/Portfolio/PortfolioApprove.vue
+++ client/components/Portfolio/PortfolioApprove.vue
@@ -2,15 +2,17 @@
 #approve_update_portfolio.main_padding
 
 	.transaction_info
-		._date 20 feb 18 16:56
-		p._value Receive 0.00364 BTS
+		p._value Send 0.00364 BTS to Trusty
+		p._value Send 0.00364 BTS to TRFND
+		p._value Send 0.00364 BTS to Email-notifier
+		p._value Sell 100 BTS for 500 EOS
 
-	trustyInput(label="value")
+	trustyInput(label="ENTER PASSWORD TO CONFIRM")
 		template(slot="input")
-			input(type="number")
+			input(v-model="password" type="password")
 
 	.trusty_inline_buttons._one_button
-		button approve
+		button CONFIRM
 
 </template>
 
@@ -21,11 +23,17 @@ export default {
   components: { trustyInput },
   data() {
     return {
-
+      password: ''
     };
   }
 };
 </script>
 
 <style lang="scss">
+p._value {
+	padding-top: 1vw;
+}
+.trusty_inline_buttons._one_button {
+	padding-top: 5vw;
+}
 </style>
