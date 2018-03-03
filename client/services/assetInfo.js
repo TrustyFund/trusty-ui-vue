d7f3d6c3f4ed557969b407360b6674f759224243
ololokiras <bellendirr@yandex.ru>
2018-03-03 18:00:18 +0800

    repair stats, add stubs

---
+++ client/services/assetInfo.js
@@ -0,0 +1,164 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["getStats"] }] */
+import axios from 'axios';
+import dateFns from 'date-fns';
+
+class AssetInfo {
+  static async initCoins() {
+    const coinsQuery = 'https://min-api.cryptocompare.com/data/all/coinlist';
+    const response = await axios.get(coinsQuery);
+    console.log('response coins', response);
+    if (response.data) {
+      return response.data.Data;
+    }
+    return false;
+  }
+
+
+  async getSocialStats(assetSymbol) {
+    if (this.coins === undefined) {
+      this.coins = await AssetInfo.initCoins();
+    }
+    if (!this.coins) {
+      return {
+        success: false,
+        error: 'coins cant downloaded'
+      };
+    }
+    const coinId = this.coins[assetSymbol].Id;
+    const socialQuery = `https://www.cryptocompare.com/api/data/socialstats/?id=${coinId}`;
+    try {
+      const socialstats = await axios.get(socialQuery);
+      if (socialstats.data.Response === 'Success') {
+        const data = {
+          symbol: socialstats.data.Data.General.Name,
+          name: socialstats.data.Data.General.CoinName,
+          twitter: socialstats.data.Data.Twitter,
+          reddit: socialstats.data.Data.Reddit,
+          facebook: socialstats.data.Data.Facebook,
+          code: []
+        };
+        socialstats.data.Data.CodeRepository.List.forEach(item => {
+          data.code.push({
+            url: item.url,
+            lastUpdate: dateFns.format(new Date(item.last_update * 1000), 'MMMM DD YYYY HH:mm:')
+          });
+        });
+        return {
+          success: true,
+          data
+        };
+      }
+      return {
+        success: false,
+        error: socialstats.data.Message
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error
+      };
+    }
+  }
+
+  async getCoinSnapshot(assetSymbol) {
+    if (this.coins === undefined) {
+      this.coins = await AssetInfo.initCoins();
+    }
+    if (!this.coins) {
+      return {
+        success: false,
+        error: 'coins cant downloaded'
+      };
+    }
+    const coinId = this.coins[assetSymbol];
+    const snapshotQuery = 'https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/' +
+      `?id=${coinId}`;
+    try {
+      const snapshotStats = await axios.get(snapshotQuery);
+      console.log(snapshotStats.data.Response);
+      if (snapshotStats.data.Response === 'Success') {
+        const data = {
+          image: `https://www.cryptocompare.com${snapshotStats.data.Data.General.ImageUrl}`,
+          description: snapshotStats.data.Data.General.Description,
+          features: snapshotStats.data.Data.General.Features,
+          technology: snapshotStats.data.Data.General.Technology,
+          totalSupply: snapshotStats.data.Data.General.TotalCoinSupply,
+          algorithm: snapshotStats.data.Data.General.Algorithm,
+          proofType: snapshotStats.data.Data.General.ProofType,
+          startDate: snapshotStats.data.Data.General.StartDate,
+          ico: {
+            status: snapshotStats.data.Data.ICO.Status,
+            whitePaper: snapshotStats.data.Data.ICO.WhitePaper
+          }
+        };
+        return {
+          success: true,
+          data
+        };
+      }
+      return {
+        success: false,
+        error: snapshotStats.data.Message
+      };
+    } catch (error) {
+      const result = {
+        success: false,
+        error
+      };
+      return result;
+    }
+  }
+
+  async getStats(fromSymbol) {
+    const statsQuery = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${fromSymbol}` +
+      '&tsyms=USD&e=CCCAGG';
+    const nowHourinMS = Date.now();
+    const nowHourQuery = `https://min-api.cryptocompare.com/data/histohour?fsym=${fromSymbol}` +
+        `&tsym=USD&limit=1&aggregate=1&toTs=${nowHourinMS}`;
+    try {
+      const statsResponse = await axios.get(statsQuery);
+      const nowResponse = await axios.get(nowHourQuery);
+
+      if (statsResponse.data.Response !== 'Error' && nowResponse.data.Response !== 'Error') {
+        const total24 = statsResponse.data.DISPLAY[fromSymbol].USD.TOTALVOLUME24HTO;
+        const marketcap = statsResponse.data.DISPLAY[fromSymbol].USD.MKTCAP;
+        const price = statsResponse.data.DISPLAY[fromSymbol].USD.PRICE;
+        const change24Percent = statsResponse.data.DISPLAY[fromSymbol].USD.CHANGEPCT24HOUR;
+
+        const { close, open } = nowResponse.data.Data[1];
+        let hourDivision;
+        let side;
+
+        if (close > open) {
+          side = '+';
+          hourDivision = close / open;
+        } else {
+          side = '-';
+          hourDivision = open / close;
+        }
+
+        const changeHourPercent = side + hourDivision.toFixed(2);
+        const stats = {
+          total24, marketcap, price, change24Percent, changeHourPercent
+        };
+        return {
+          success: true,
+          data: stats
+        };
+      }
+      return {
+        success: false,
+        error: statsResponse.data.Message
+      };
+    } catch (error) {
+      const result = {
+        success: false,
+        error
+      };
+      return result;
+    }
+  }
+}
+
+export default new AssetInfo();
+
