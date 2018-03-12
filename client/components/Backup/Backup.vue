8fc382422ea42ac02f67db42a99fafe4c8cd529e
riffca <riffca@yandex.ru>
2018-03-12 12:49:31 +0300

    change stretch logic

--- client/components/Backup/Backup.vue
+++ client/components/Backup/Backup.vue
@@ -219,6 +219,11 @@ $light_grey: #6d6e70;
 	}
 
 	.verify_area {
+		min-height: 28.6vh;
+		max-height: 52.6vh;
+		margin-bottom: 5.3vw;
+		border-top: 1px dashed white;
+		border-bottom: 1px dashed white;
 		overflow: scroll;
 		span {
 			cursor: pointer;
@@ -226,6 +231,9 @@ $light_grey: #6d6e70;
 			margin: 1.4vw;
 			border: 1px dashed white;
 			padding: 1.5vw 3vw;
+		}
+		p {
+			margin-bottom:1.2vw;
 		}
 	}
 
@@ -246,16 +254,6 @@ $light_grey: #6d6e70;
 			border: 1px solid white;
 			padding: 1.5vw 3vw;
 		}
-	}
-
-	.verify_area {
-		height: 28.6vh;
-		max-height: 52.6vh;
-		margin-bottom: 5.3vw;
-		border-top: 1px dashed white;
-		border-bottom: 1px dashed white;
-		//overflow: scroll;
-
 	}
 
 	@media screen and (min-width: 769px) {
@@ -356,7 +354,8 @@ $light_grey: #6d6e70;
 		}
 
 		.verify_area {
-			height: 28.6vh;
+			min-height: 28.6vh;
+			max-height: 52.6vh;
 			margin-bottom: px_from_vw(5.3);
 
 		}
