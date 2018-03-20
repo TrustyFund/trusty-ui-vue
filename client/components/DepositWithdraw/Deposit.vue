95d2a83a8520e25968cb6c934e151ded479bf2e4
Anton <anlopan@gmail.com>
2018-03-20 22:14:34 +0300

    Base component

--- client/components/DepositWithdraw/Deposit.vue
+++ client/components/DepositWithdraw/Deposit.vue
@@ -12,14 +12,14 @@
         select(v-model="selectedcoin")
           option(v-for="coin in coins") {{ coin }}
 
-    trusty-input(:isOpen="true", label="payment method" className="select_input")
+    trusty-input(:isOpen="true", label="payment method" className="select_input" )
       template(slot="input")
         input(:style="{display:'none'}")
-        select(v-model="service")
+        select(v-model="paymentmethod" )
           option(v-for="method in methods", :value="method") {{ method }}
 
   ._turnover_service
-    component(:is="service")
+    component(:is="gateway" :payload="selectedcoin")
 
 </template>
 
@@ -27,44 +27,35 @@
 import trustyInput from '@/components/UI/form/input';
 import iconComponent from '@/components/UI/icon';
 import openledger from './Openledger/Deposit';
+import trusty from './Trusty/Deposit';
 import './style.scss';
 
 const methodsByGate = {
   trusy: ['SBERBANK', 'TINKOFF'],
-  openledger: ['openledger']
+  openledger: ['openledger', 'sdasdsad']
 };
 
 export default {
-  components: { trustyInput, iconComponent, openledger },
+  components: { trustyInput, iconComponent, openledger, trusty },
   computed: {
     gateway() {
       if (this.selectedcoin === 'RUB') {
-        return 'trusy';
+        return 'trusty';
       }
       return 'openledger';
     },
     methods() {
       return methodsByGate[this.gateway];
-    },
-    depositAddress() {
-      const exists = this.$store.state.transfer.depositAddress;
-      const address = exists ? exists.address : null;
-      if (address) {
-        const firstCount = Math.floor(address.length / 2) - 1;
-        const start = address.slice(0, firstCount);
-        const end = address.slice(firstCount);
-        return `<span>${start}</span><br/><span>${end}</span>`;
-      }
-      return '<span>no address</span>';
     }
   },
   data() {
     return {
       selectedcoin: 'BTC',
-      service: 'openledger',
+      paymentmethod: 'openledger',
       amount: '',
       coins: ['BTC', 'ETH', 'RUB']
     };
   }
 };
 </script>
+
--- client/components/DepositWithdraw/Openledger/Deposit.vue
+++ client/components/DepositWithdraw/Openledger/Deposit.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 ._turnover_info
   .trusty_help_text._yellow
-    | Send {{ selectedcoin }} to the address below
+    | Send {{ payload }} to the address below
   .trusty_cutted_address(v-html="depositAddress")
   .trusty_inline_buttons._one_button: button Copy address
   .trusty_help_text._yellow
     | Push CONFIRM button as soon as you have completed the payment
   .trusty_inline_buttons
-    button Confirm
-    button Cancel
+    button(v-on:click="test2") Confirm
+    button(v-on:click="test") Cancel
   p.trusty_ps_text
     | Payment gateway service is provided by #[br]
     | Openledger.io at 0% fee
@@ -17,11 +17,54 @@
 
 <script>
 import trustyInput from '@/components/UI/form/input';
+import { mapGetters, mapActions } from 'vuex';
 import iconComponent from '@/components/UI/icon';
+import API from 'lib/src/services/api';
 
 export default {
+  props: ['payload'],
   data() {
     return {};
+  },
+  beforeMount() {
+    this.fetchAddress({ asset: this.payload });
+  },
+  computed: {
+    depositAddress() {
+      const address = this.address;
+      /* if (address) {
+        const firstCount = Math.floor(address.length / 2) - 1;
+        const start = address.slice(0, firstCount);
+        const end = address.slice(firstCount);
+        return `<span>${start}</span><br/><span>${end}</span>`;
+      } */
+      return `<span>${address}</span>`;
+    },
+    ...mapGetters({
+      address: 'openledger/getDepositAddress'
+    })
+  },
+  methods: {
+    ...mapActions({
+      fetchAddress: 'openledger/fetchDepositAddress'
+    }),
+    test() {
+      console.log('REQ ADDR');
+      API.Openledger.requestDepositAdress({
+        asset: this.payload,
+        user: 'hobb1t'
+      }).then((result) => {
+        console.log('RESULT', result);
+      });
+    },
+    test2() {
+      API.Openledger.getLastDepositAdress({
+        asset: this.payload,
+        user: 'hobb1t'
+      }).then((result) => {
+        console.log('RESULT', result);
+      });
+    }
   },
   components: {
     trustyInput,
@@ -29,3 +72,9 @@ export default {
   }
 };
 </script>
+
+<style>
+#trusty_transfer ._input_space input{
+  width: 75%!important;
+}
+</style>
