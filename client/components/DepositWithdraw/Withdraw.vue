80cda982a5a0d3addd0062107a15497a3dbb4332
Anton <anlopan@gmail.com>
2018-03-30 13:51:19 +0300

    TransferFee

--- client/components/DepositWithdraw/Withdraw.vue
+++ client/components/DepositWithdraw/Withdraw.vue
@@ -66,7 +66,9 @@ export default {
       doesntExceedBalance(value) {
         const id = this.selectedCoin;
         const balance = this.balances[id].balance / (10 ** this.getAssetById(id).precision);
-        return (value * 1.03) < balance;
+
+        console.log('Check', value, balance);
+        return (value) <= balance;
       },
       doesntExceedMinWithdraw(value) {
         if (this.paymentMethod === OpenledgerName) {
