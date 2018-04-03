272d03aa49e6fa7b02b7c48e07c8f4ca720956a6
Рома <roma219@yandex.ru>
2018-04-03 15:57:54 +0800

    0 orders basic handling

--- client/components/ConfirmTransactions/ConfirmTransactions.vue
+++ client/components/ConfirmTransactions/ConfirmTransactions.vue
@@ -176,6 +176,12 @@ export default {
   },
   beforeDestroy() {
     this.removePendingDistribution();
+  },
+  created() {
+    if (!this.hasOrders && !this.hasPendingTransfer) {
+      this.$router.go(-1);
+      this.$toast.error('Unable to create orders, try again');
+    }
   }
 };
 </script>
