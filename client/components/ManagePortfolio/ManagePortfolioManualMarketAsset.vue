bf2c2532910012452ef8b109f61e3c78d117dcc3
Anton <anlopan@gmail.com>
2018-04-06 00:58:28 +0300

    Show order book

--- client/components/ManagePortfolio/ManagePortfolioManualMarketAsset.vue
+++ client/components/ManagePortfolio/ManagePortfolioManualMarketAsset.vue
@@ -1,9 +1,15 @@
 <template lang="pug">
 .manual_trading_market_asset Market {{ getAssetById(market).symbol }} : {{ getAssetById(asset).symbol }}
+  .orders.sell_orders
+    template(v-for="order in sellOrders")
+      p {{ order.id }}
+  .orders.buy_orders
+    template(v-for="order in buyOrders")
+      p {{ order.id }}
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   props: ['market', 'asset'],
@@ -13,8 +19,37 @@ export default {
   },
   computed: {
     ...mapGetters({
-      getAssetById: 'assets/getAssetById'
+      getAssetById: 'assets/getAssetById',
+      getMarketOrders: 'market2/getMarketOrders'
+    }),
+    buyOrders() {
+      return this.getMarketOrders(this.market)[this.asset].buy;
+    },
+    sellOrders() {
+      return this.getMarketOrders(this.market)[this.asset].sell;
+    }
+  },
+  methods: {
+    ...mapActions({
+      subscribeToMarket: 'market2/subscribeToMarket',
+      unsubscribeFromMarket: 'market2/unsubscribeFromMarket'
     })
+  },
+  created() {
+    const baseId = this.market;
+    const assetId = this.asset;
+    this.subscribeToMarket({ baseId, assetId });
+  },
+  beforeDestroy() {
+    this.unsubscribeFromMarket({ baseId: this.market });
   }
 };
 </script>
+
+<style>
+.manual_trading_market_asset .orders {
+  height: 40vh;
+  width: 100vw;
+  overflow-y: scroll;
+}
+</style>
